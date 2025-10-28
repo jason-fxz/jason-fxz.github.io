7a7d27e1f891becf224ea8f8c897f9f102b66ef6
jason-fxz <jason341132@qq.com>
2025-10-28 08:59:54 +0000

    Deploying to gh-pages from @ jason-fxz/jason-fxz.github.io@1e8afc7625d2bd467a58c2024d2fb3d1eec9588c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,25 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "post-pd-multiplexing-unlocking-high-goodput-llm-serving-with-greencontext-lmsys-org",
+        
+          title: 'PD-Multiplexing: Unlocking High-Goodput LLM Serving with GreenContext | LMSYS Org <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "This post highlights our initial efforts to support a new serving paradigm, PD-Multiplexing, in SGLang. It is designed t...",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://lmsys.org/blog/2025-09-28-pdmux/", "_blank");
+          
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
